60e274f6cc4d1f46ccda15590e51e37168aa5bdd
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-08-15 14:18:53 +1000

    clean up run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
-  1 #!/bin/bash                                                                                                        
-  2                                                                                                                    
-  3 set -x                                                                                                             
-  4                                                                                                                    
-  5 python fine_tuning.py fit --config configs/dino_vitb16_n_splits_3-no_mix-lr_005.yaml
-  6 python fine_tuning.py fit --config configs/dino_vitb16-n_splits_1-mix-lr_005.yaml
-  7 python fine_tuning.py fit --config configs/dino_vitb16-n_splits_3-mix-lr_005.yaml
-  
+#!/bin/bash
+
+set -x
+
+python fine_tuning.py fit --config configs/dino_vitb16_n_splits_3-no_mix-lr_005.yaml
+python fine_tuning.py fit --config configs/dino_vitb16-n_splits_1-mix-lr_005.yaml
+python fine_tuning.py fit --config configs/dino_vitb16-n_splits_3-mix-lr_005.yaml
