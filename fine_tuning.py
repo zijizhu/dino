02127e5f03c0cb23be14c7266991d5789f918046
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-08-15 12:51:09 +1000

    copy get_cosine_schedule_with_warmup from transformers and remove it from imports

--- fine_tuning.py
+++ fine_tuning.py
@@ -1,3 +1,4 @@
+import math
 import re
 from functools import partial
 
@@ -9,12 +10,55 @@ import torchvision.transforms.v2 as v2
 from lightning.pytorch.cli import LightningCLI
 from timm.loss.cross_entropy import SoftTargetCrossEntropy
 from torch import nn
-from torch.optim import SGD
+from torch.optim import SGD, Optimizer
+from torch.optim.lr_scheduler import LambdaLR
 from torchmetrics.classification.accuracy import Accuracy
-from transformers.optimization import get_cosine_schedule_with_warmup
 
 from data import DataModule
 from vision_transformer import VisionTransformer
+
+
+def _get_cosine_schedule_with_warmup_lr_lambda(
+    current_step: int, *, num_warmup_steps: int, num_training_steps: int, num_cycles: float
+):
+    if current_step < num_warmup_steps:
+        return float(current_step) / float(max(1, num_warmup_steps))
+    progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
+    return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
+
+
+def get_cosine_schedule_with_warmup(
+    optimizer: Optimizer, num_warmup_steps: int, num_training_steps: int, num_cycles: float = 0.5, last_epoch: int = -1
+):
+    """
+    Create a schedule with a learning rate that decreases following the values of the cosine function between the
+    initial lr set in the optimizer to 0, after a warmup period during which it increases linearly between 0 and the
+    initial lr set in the optimizer.
+
+    Args:
+        optimizer ([`~torch.optim.Optimizer`]):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (`int`):
+            The total number of training steps.
+        num_cycles (`float`, *optional*, defaults to 0.5):
+            The number of waves in the cosine schedule (the defaults is to just decrease from the max value to 0
+            following a half-cosine).
+        last_epoch (`int`, *optional*, defaults to -1):
+            The index of the last epoch when resuming training.
+
+    Return:
+        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    lr_lambda = partial(
+        _get_cosine_schedule_with_warmup_lr_lambda,
+        num_warmup_steps=num_warmup_steps,
+        num_training_steps=num_training_steps,
+        num_cycles=num_cycles,
+    )
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
 
 
 def block_expansion_dino(state_dict: dict[str, torch.Tensor], n_splits: int = 3):
